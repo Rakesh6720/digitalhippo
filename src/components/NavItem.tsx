0fed44c9d3effea79eef118c9b6a7b2b648097c2
Rakesh Baruah <rbaruah@gmail.com>
2023-12-04 08:54:10 -0600

    add nav bar contents

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,6 +4,8 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { ChevronDown } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = (typeof PRODUCT_CATEGORIES)[number];
 
@@ -36,6 +38,53 @@ export default function NavItem({
           />
         </Button>
       </div>
+      {isOpen ? (
+        <div
+          className={cn(
+            "absolute inset-x-0 top-full text-sm text-muted-foreground",
+            {
+              "animate-in fade-in-10 slide-in-from-top-5": !isOpen,
+            }
+          )}
+        >
+          <div
+            className="absolute inset-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          />
+          <div className="relative bg-white">
+            <div className="mx-auto m-w-7xl px-8">
+              <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                  {category.featured.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative texst-base sm:text-sm"
+                    >
+                      <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                        <Image
+                          src={item.imageSrc}
+                          alt="product category image"
+                          fill
+                          className="object-cover object-center"
+                        />
+                        <Link
+                          href={item.href}
+                          className="mt-6 bock font-medium text-gray-900"
+                        >
+                          {item.name}
+                        </Link>
+                        <p className="mt-1" aria-hidden="true">
+                          Shop now
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
